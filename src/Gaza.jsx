afe4f5fa72242134a9d99de91a4e8c7008704df3
muhammad45401 <saadjani3333@gmail.com>
2024-06-28 17:19:33 +0500

    react dawn project

---
+++ src/Gaza.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+
+
+
+function Gaza(){
+    return (
+   <div className="App1">
+ <h3>DAWN</h3>
+ <h1>Israel's Gaza invasion - Day 266</h1>
+
+
+ <div className='app2'>
+ <div className='app2-ch1'>
+   <ul>
+    <li>Israel’s siege of Gaza Strip, sparked by Hamas’ Oct 7 attack, enters ninth month</li>
+    <li>Israeli strikes have laid waste to much of Gaza strip and caused major environmental damage</li>
+    <li>Starving Palestinians surviving on less than 3pc of daily water requirements while 67pc of water, sanitation infrastructure destroyed</li>
+    <li>More than 250 killed in Israeli raid on Nuseirat refugee camp to rescue 4 hostages, days after a strike on UN school killed at least 35</li>
+    <li>Truce not secured despite UNSC adopting resolution based on ceasefire plan announced by Biden</li></ul> 
+ </div>
+   
+   <div className='app2-ch2'><img src="pic/pic.jpg"></img></div></div>
+
+
+
+
+   </div>
+
+  
+  
+ 
+   
+   
+    ) 
+}
+export default Gaza;
